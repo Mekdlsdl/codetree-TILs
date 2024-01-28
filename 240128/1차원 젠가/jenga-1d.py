2c5a970d5338d49af34bf1420774f6442cf0c421
Mekdlsdl <114468322+Mekdlsdl@users.noreply.github.com>
2024-01-28 22:07:47 +0900

    Title: 1차원 젠가, Time: 123ms, Memory: 24MB, Status: Passed - CodeTree

--- 240128/1차원 젠가/jenga-1d.py
+++ 240128/1차원 젠가/jenga-1d.py
@@ -6,19 +6,19 @@ s2, e2 = map(int, input().split())
 temp = [0 for _ in range(n)]
 cnt = 0
 for i in range(n):
-    if i >= s1 - 1 and i <= e1 - 1:
-        continue
-    temp[cnt] = block[i]
-    cnt += 1
+    if i < s1 - 1 or i > e1 - 1:
+        temp[cnt] = block[i]
+        cnt += 1
+    
 
 temp1 = [0 for _ in range(cnt)]
 
 cnt1 = 0
 for i in range(cnt):
-    if i >= s2 - 1 and i <= e2 - 1:
-        continue
-    temp1[cnt1] = temp[i]
-    cnt1 += 1
+    if i < s2 - 1 or i > e2 - 1:
+        temp1[cnt1] = temp[i]
+        cnt1 += 1
+    
 
 print(cnt1)
 for t in temp1:
