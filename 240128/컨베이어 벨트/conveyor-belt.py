52a379565c676bf2b2bf483bd0dc67e827cd5ffb
Mekdlsdl <114468322+Mekdlsdl@users.noreply.github.com>
2024-01-28 20:18:26 +0900

    Title: 컨베이어 벨트, Time: 120ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240128/컨베이어 벨트/conveyor-belt.py
+++ 240128/컨베이어 벨트/conveyor-belt.py
@@ -4,12 +4,14 @@ arr = [list(map(int, input().split())) for _ in range(2)]
 
 arr1 = []
 
-for i in range(2):
-    for j in range(n):
-        arr1.append(arr[i][j])
+while (t>0):
+    for i in range(2):
+        for j in range(n):
+            arr1.append(arr[i][j])
 
+    temp = arr1[-1]
 
-temp = arr1[-1]
+    t -= 1
 
 for i in range(len(arr1) - 1, 0, -1):
     arr1[i] = arr1[i - 1]
