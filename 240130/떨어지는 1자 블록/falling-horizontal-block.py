b8b219842baa38c146da7da2b076783abd2e97e0
Mekdlsdl <114468322+Mekdlsdl@users.noreply.github.com>
2024-01-30 23:13:25 +0900

    Title: 떨어지는 1자 블록, Time: 129ms, Memory: 25MB, Status: Passed - CodeTree

--- 240130/떨어지는 1자 블록/falling-horizontal-block.py
+++ 240130/떨어지는 1자 블록/falling-horizontal-block.py
@@ -2,7 +2,7 @@ n, m, k = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(n)]
 
 def drop_block():
-    floor = n + 1
+    floor = -1
     s, e = k - 1, k + m - 1
     for i in range(n):
         for j in range(s, e):
@@ -10,11 +10,8 @@ def drop_block():
                 floor = i - 1
                 break
 
-        if floor != n + 1:
+        if floor != -1:
             break
-
-    if floor == n + 1:
-        floor = n - 1
 
     for i in range(s, e):
         arr[floor][i] = 1
