031a35929913994f62946598845ef733264cfd02
문다인 <114468322+Mekdlsdl@users.noreply.github.com>
2025-10-23 11:25:17 +0900

    Title: 점수 비교, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 251023/점수 비교/score-comparison.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int am, bm, ae, be;
+    cin >> am >> bm >> ae >> be;
+
+    cout << ((am > bm) && (ae > be));
+    
+    return 0;
+}
