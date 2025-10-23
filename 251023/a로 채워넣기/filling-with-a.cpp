1c318e63a789ea5b15ad532abf1bcee74843f2e4
문다인 <114468322+Mekdlsdl@users.noreply.github.com>
2025-10-23 12:17:55 +0900

    Title: a로 채워넣기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251023/a로 채워넣기/filling-with-a.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    string s;
+    cin >> s;
+
+    s[1] = 'a';
+    s[s.length() - 2] = 'a';
+
+    cout << s;
+
+    return 0;
+}
