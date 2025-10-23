63da29c6a1b6d725485e8ca564f09d306427c872
문다인 <114468322+Mekdlsdl@users.noreply.github.com>
2025-10-23 10:59:55 +0900

    Title: 물의 상태, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251023/물의 상태/state-of-water.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int t;
+    cin >> t;
+
+    if (t < 0) cout << "ice";
+    else if (t >= 100) cout << "vapor";
+    else cout << "water";
+    
+    return 0;
+}
