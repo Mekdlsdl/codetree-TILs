7b5c5287c417c376b3dce69b643b2f8067d92597
문다인 <114468322+Mekdlsdl@users.noreply.github.com>
2025-10-23 20:38:44 +0900

    Title: 숫자 피라미드 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251023/숫자 피라미드 2/number-pyramid-2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin >> n;
+
+    int num = 0;
+
+    for (int i = 0; i < n; i++) {
+        
+        for (int j = 0; j < i + 1; j++) {
+            num++;
+            cout << num << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
