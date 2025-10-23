05a0768a3bae3dd7b1f1649a67cf75ef4c8fb9ed
문다인 <114468322+Mekdlsdl@users.noreply.github.com>
2025-10-23 11:43:25 +0900

    Title: 남녀노소 구분짓기, Time: 9ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251023/남녀노소 구분짓기/sex-and-age.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int g, a;
+    cin >> g >> a;
+
+    if (g == 0) {
+        if (a >= 19) cout << "MAN";
+        else cout << "BOY";
+    }
+    else {
+        if (a >= 19) cout << "WOMAN";
+        else cout << "GIRL";
+    } 
+
+    return 0;
+}
