f8dbf025ddf5c8024399d3c10f781540fd455cf7
문다인 <114468322+Mekdlsdl@users.noreply.github.com>
2025-10-23 11:27:30 +0900

    Title: 점수 비교, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251023/점수 비교/score-comparison.cpp
+++ 251023/점수 비교/score-comparison.cpp
@@ -4,9 +4,9 @@ using namespace std;
 int main() {
     // Please write your code here.
     int am, bm, ae, be;
-    cin >> am >> bm >> ae >> be;
+    cin >> am >> ae >> bm >> be;
 
-    cout << ((am >= bm) && (ae >= be));
+    cout << ((am > bm) && (ae > be));
 
     return 0;
 }
