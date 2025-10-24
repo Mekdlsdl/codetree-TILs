9f7fa8c623971691cd7b7499ae95b022918c2605
문다인 <114468322+Mekdlsdl@users.noreply.github.com>
2025-10-25 01:04:35 +0900

    Title: 방향에 맞춰 이동, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 251025/방향에 맞춰 이동/move-in-direction.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+int n;
+char dir[100];
+int dist[100];
+
+int main() {
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> dir[i] >> dist[i];
+    }
+
+    // Please write your code here.
+    map<char, int> m = {{'N', 0}, {'E', 1}, {'S', 2}, {'w', 3}};
+    int dx[4] = { 0, 1, 0, -1 }, dy[4] = { 1, 0, -1, 0 };
+    int x = 0, y = 0;
+
+    for (int i = 0; i < n; i++) {
+        char d = dir[i];
+        int t = dist[i];
+
+        x = x + dx[m[d]] * t;
+        y = y + dy[m[d]] * t;
+    }
+
+    cout << x << " " << y;
+
+    return 0;
+}
