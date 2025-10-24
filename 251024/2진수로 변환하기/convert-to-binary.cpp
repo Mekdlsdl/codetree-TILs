89542c70403be1a9536de4cfed750608cb8a6c44
문다인 <114468322+Mekdlsdl@users.noreply.github.com>
2025-10-24 11:29:18 +0900

    Title: 2진수로 변환하기, Time: 320ms, Memory: 80MB, Status: Memory Limit Exceed - Codetree

---
+++ 251024/2진수로 변환하기/convert-to-binary.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int n;
+
+int main() {
+    cin >> n;
+
+    // Please write your code here.
+    vector<int> ans;
+
+    while (true) {
+        if (n == 1) {
+            ans.push_back(n);
+            break;
+        }
+
+        ans.push_back(n % 2);
+        n /= 2;
+    }
+
+    for (int j = ans.size() - 1; j >= 0; j--) {
+        cout << ans[j];
+    }
+
+    return 0;
+}
