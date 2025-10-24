b07d8220ea093490f097b1e3e2f24f946f84bbc4
문다인 <114468322+Mekdlsdl@users.noreply.github.com>
2025-10-24 23:02:04 +0900

    Title: 연속되는 수 2, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 251024/연속되는 수 2/continuous-number2.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int N;
+int arr[1000];
+
+int main() {
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        cin >> arr[i];
+    }
+
+    // Please write your code here.
+    int ans = 0;
+    int cnt = 0;
+
+    for (int i = 0; i < N; i++) {
+        cnt++;
+
+        if (i == 0 || arr[i - 1] != arr[i]) {
+            ans = max(ans, cnt);
+            cnt = 0;
+        }
+    }
+
+    cout << ans;
+
+    return 0;
+}
