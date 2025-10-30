cca5dd92d387f93c1771829de4b69bc1420e6b1a
문다인 <114468322+Mekdlsdl@users.noreply.github.com>
2025-10-30 12:49:04 +0900

    Title: 최대 증가 부분 수열, Time: 61ms, Memory: 5MB, Status: Wrong Answer - Codetree

---
+++ 251030/최대 증가 부분 수열/longest-increasing-subsequence.cs
@@ -0,0 +1,42 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        // Please write your code here.
+        int n = int.Parse(Console.ReadLine());
+        int[] arr = new int[n];
+
+        int idx = 0;
+
+        while (idx < n) {
+            string line = Console.ReadLine();
+
+            foreach (string l in line.Split()) {
+                arr[idx++] = int.Parse(l);
+                if (idx == n) break;
+            }
+        }
+
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+
+        for (int i = 1; i < n; i++) {
+            for (int j = i - 1; j >= 0; j--) {
+                if (arr[i] > arr[j]) {
+                    dp[i] = dp[j] + 1;
+                    break;
+                }
+            }
+        }
+
+        int ans = -1;
+        for (int i = 0; i < n; i++) {
+            // Console.Write($"{dp[i]} ");
+            ans = Math.Max(ans, dp[i]);
+        }
+
+        Console.Write(ans);
+    }
+}
