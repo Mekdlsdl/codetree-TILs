901ee8e0957f5ab6684bf80bf81b096d047a4430
문다인 <114468322+Mekdlsdl@users.noreply.github.com>
2025-11-21 20:39:56 +0900

    Title: 알파벳과 사칙연산, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 251121/알파벳과 사칙연산/calculations-with-alphabet.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+string expression;
+
+map<char, int> alpha;
+int selected[101];
+
+int n;
+int ans = 0;
+
+
+void Calculate() {
+    int result = 0;
+    char op = '+';
+
+    for (int i = 0; i < expression.size(); i++) {
+        if (i % 2 == 1) continue;
+
+        switch (op) {
+            case '+':
+                result += alpha[expression[i]];
+                break;
+            case '-':
+                result -= alpha[expression[i]];
+                break;
+            case '*':
+                result *= alpha[expression[i]];
+                break;
+        }
+
+        op = expression[i + 1];
+    }
+
+    ans = max(result, ans);
+}
+
+void Choose(int cnt) {
+    if (cnt == alpha.size()) {
+        int idx = 0;
+
+        for (auto& k : alpha) {
+            alpha[k.first] = selected[idx];
+            idx++;
+        }
+
+        Calculate();
+
+        return;
+    }
+
+    for (int i = 1; i <= 4; i++) {
+        selected[cnt] = i;
+        Choose(cnt + 1);
+    }
+}
+
+int main() {
+    cin >> expression;
+
+    n = expression.size();
+
+    // Please write your code here.
+    for (int i = 0; i < n; i++) {
+        if (i % 2 == 0) {
+            alpha[expression[i]] = 0;
+        }
+    }
+
+    Choose(0);
+
+    cout << ans;
+
+    return 0;
+}
