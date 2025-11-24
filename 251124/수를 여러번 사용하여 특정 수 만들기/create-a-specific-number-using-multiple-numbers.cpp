27422730c0a17e66951dc5a0f5dea2b97fbba58a
문다인 <114468322+Mekdlsdl@users.noreply.github.com>
2025-11-24 20:01:26 +0900

    Title: 수를 여러번 사용하여 특정 수 만들기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251124/수를 여러번 사용하여 특정 수 만들기/create-a-specific-number-using-multiple-numbers.cpp
+++ 251124/수를 여러번 사용하여 특정 수 만들기/create-a-specific-number-using-multiple-numbers.cpp
@@ -5,13 +5,16 @@ using namespace std;
 
 int A, B, C;
 int ans = 0;
+bool visited[1001] = {false};
 
 void Choose(int num) {
-    if (num > C) { 
-        return;
-    }
+    if (num > C) return;
 
+    if (visited[num]) return;
+    visited[num] = true;
+    
     ans = max(ans, num);
+    
 
     Choose(num + A);
     Choose(num + B);
